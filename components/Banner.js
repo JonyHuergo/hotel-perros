26fdcbbeac755520a4080e4d505fcc198222249f
Jonathan Huergo <jh2000@hotmail.com.ar>
2022-12-17 04:24:36 -0300

    Add placeholder banner text

--- components/Banner.js
+++ components/Banner.js
@@ -2,6 +2,14 @@ const Banner = () => {
     return (
         <section className="banner">
             <img src="/banner-img.jpg" alt="Banner" className="banner-img"/>
+            <div className="banner-text">
+                <h2>ÚNICO EN ARGENTINA</h2>
+                <h1>Hotel de perros</h1>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil veniam corporis fugit iusto suscipit voluptatum, 
+                    tempore minima cupiditate vel tempora consectetur quia fuga debitis laudantium a soluta dolorem fugiat dolor.
+                </p>
+            </div>
         </section>
     );
 }
