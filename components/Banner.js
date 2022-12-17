214d7220aad84bcb4906bcc09c55a3f4d3d06afa
Jonathan Huergo <jh2000@hotmail.com.ar>
2022-12-17 04:06:03 -0300

    Add Banner component
    Added the Banner component. Added the banner image.

---
+++ components/Banner.js
@@ -0,0 +1,9 @@
+const Banner = () => {
+    return (
+        <section className="banner">
+            <img src="/banner-img.jpg" alt="Banner" className="banner-img"/>
+        </section>
+    );
+}
+ 
+export default Banner;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
+import Banner from '../components/Banner'
 
 export default function Home() {
   return (
+    <>
     <Head>
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -9,5 +11,7 @@ export default function Home() {
       <meta name="description" content="Estadia para perros" />
       {/* <link rel="icon" href="/favicon.ico" /> */}
     </Head>
+    <Banner/>
+    </>
   )
 }
