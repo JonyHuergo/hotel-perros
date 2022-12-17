1af630ff4a828295b0718c28bbad969c468374af
Jonathan Huergo <jh2000@hotmail.com.ar>
2022-12-17 04:32:02 -0300

    Add Reasons component

---
+++ components/Reasons.js
@@ -0,0 +1,23 @@
+const Reasons = () => {
+    return (
+        <section>
+            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, veritatis.</h2>
+            <article>
+                <h3>Lorem ipsum dolor sit.</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Inventore cum et amet a voluptatum? Est officiis sit consequuntur vitae natus.
+                </p>
+            </article>
+            <article>
+                <h3>Lorem ipsum dolor sit.</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Inventore cum et amet a voluptatum? Est officiis sit consequuntur vitae natus.
+                </p>
+            </article>
+        </section>
+    );
+}
+ 
+export default Reasons;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
+import Reasons from '../components/Reasons'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       {/* <link rel="icon" href="/favicon.ico" /> */}
     </Head>
     <Banner/>
+    <Reasons/>
     </>
   )
 }
